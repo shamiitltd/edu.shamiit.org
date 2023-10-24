d1a8ec5bdeff006f3dbf26fcd931de1103190bd8
Satyam Gupta <satyamguptads3@gmail.com>
2023-10-24 12:09:21 +0530

    add new validator

--- app/Http/Controllers/SmVisitorController.php
+++ app/Http/Controllers/SmVisitorController.php
@@ -11,6 +11,7 @@ use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Validator;
 
 class SmVisitorController extends Controller
 {
