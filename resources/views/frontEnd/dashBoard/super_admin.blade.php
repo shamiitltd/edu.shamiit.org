17adaab13022f99fb3c0cd002e382dd3f23e9149
Mohit Yadav <mohityadav12181@gmail.com>
2023-10-26 21:07:06 +0530

    none

--- resources/views/frontEnd/dashBoard/super_admin.blade.php
+++ resources/views/frontEnd/dashBoard/super_admin.blade.php
@@ -6,7 +6,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="main-title">
-                        <h3 class="mb-30">Welcome Super Admin  Welcome</h3>
+                        <h3 class="mb-30">Welcome Super Admin </h3>
                     </div>
                 </div>
             </div>
