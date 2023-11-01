9f379a6d4ea85cdc1b19d01b665f98dd942a04e8
mesnehadutta <111640554+mesnehadutta@users.noreply.github.com>
2023-11-01 10:50:14 +0530

    logo_img

--- database/migrations/2020_03_14_123955_create_sms_templates_table.php
+++ database/migrations/2020_03_14_123955_create_sms_templates_table.php
@@ -104,7 +104,7 @@ class CreateSmsTemplatesTable extends Migration
                                             <div class="col_cont" style="width:100%;">
                                                 <div align="center" class="img-container center fixedwidth" style="padding-right:30px;padding-left:30px;">
                                                     <a href="#">
-                                                        <img border="0" class="center fixedwidth " src="../../public/backEnd/img/logo.png" style="padding-top:30px;padding-bottom:30px;text-decoration:none;height:auto;border:0;max-width:100px;" width="100" alt="logo.png">
+                                                        <img border="0" class="center fixedwidth img-container" src="../../public/backEnd/img/logo.png" style="padding-top:30px;padding-bottom:30px;text-decoration:none;height:auto;border:0;max-width:100%;" width="100" alt="logo.png">
                                                     </a>
                                                 </div>
                                             </div>
