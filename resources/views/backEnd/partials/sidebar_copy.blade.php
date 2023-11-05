702b89f9e2c21982725f71ec569c32e207c4a5ec
devangi <devangihrathod@gmail.com>
2023-11-05 19:08:44 +0530

    change_title_leve

--- resources/views/backEnd/partials/sidebar_copy.blade.php
+++ resources/views/backEnd/partials/sidebar_copy.blade.php
@@ -841,7 +841,7 @@
                                 @endif
                                 @if (userPermission(199) && menuStatus(199))
                                     <li data-position="{{ menuPosition(199) }}">
-                                        <a href="{{ route('leave-define') }}"> @lang('leave.Add_leave')</a>
+                                        <a href="{{ route('leave-define') }}"> @lang('leave.leave_define')</a>
                                     </li>
                                 @endif
                                 @if (userPermission(189) && menuStatus(189))
