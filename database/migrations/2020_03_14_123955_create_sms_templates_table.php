b022ab25128b93e9144e1936bb16436319d14686
mesnehadutta <111640554+mesnehadutta@users.noreply.github.com>
2023-11-11 19:45:55 +0530

    Last Changes

--- database/migrations/2020_03_14_123955_create_sms_templates_table.php
+++ database/migrations/2020_03_14_123955_create_sms_templates_table.php
@@ -130,9 +130,9 @@ class CreateSmsTemplatesTable extends Migration
                                     <div style="border-collapse:collapse;width:100%;background-color:#ffffff;">
                                         <div class="col num12" style="min-width:320px;max-width:600px;vertical-align:top;width:600px;">
                                             <div class="col_cont" style="width:100%;">
-                                                <h1 style="line-height:120%;text-align:center;margin-bottom:0px;">
+                                                <h1 style="line-height:120%;text-align:center;margin-bottom:0px;margin-top:20px;">
                                                     <font color="#555555" face="Arial, Helvetica Neue, Helvetica, sans-serif">
-                                                        <span style="font-size:36px; margin-top:20px;">Test Mail1</span>
+                                                        <span style="font-size:36px; margin-top:20px;">Test Mail</span>
                                                     </font>
                                                 </h1>
                                                 <div style="line-height:1.8;padding:20px 15px;">
@@ -217,7 +217,7 @@ class CreateSmsTemplatesTable extends Migration
                                         <div style="border-collapse:collapse;width:100%;background-color:#ffffff;">
                                             <div class="col num12" style="min-width:320px;max-width:600px;vertical-align:top;width:600px;">
                                                 <div class="col_cont" style="width:100%;">
-                                                    <h1 style="line-height:120%;text-align:center;margin-bottom:0px;">
+                                                    <h1 style="line-height:120%;text-align:center;margin-bottom:0px;margin-top:20px;">
                                                         <font color="#555555" face="Arial, Helvetica Neue, Helvetica, sans-serif">
                                                             <span style="font-size:36px;">Password Reset</span>
                                                         </font>
@@ -319,7 +319,7 @@ class CreateSmsTemplatesTable extends Migration
                                     <div style="border-collapse:collapse;width:100%;background-color:#ffffff;">
                                         <div class="col num12" style="min-width:320px;max-width:600px;vertical-align:top;width:600px;">
                                             <div class="col_cont" style="width:100%;">
-                                                <h1 style="line-height:120%;text-align:center;margin-bottom:0px;">
+                                                <h1 style="line-height:120%;text-align:center;margin-bottom:0px;margin-top:20px;">
                                                     <font color="#555555" face="Arial, Helvetica Neue, Helvetica, sans-serif">
                                                         <span style="font-size:36px;">Student Login Credentials</span>
                                                     </font>
@@ -342,6 +342,11 @@ class CreateSmsTemplatesTable extends Migration
                                     <div style="border-collapse:collapse;width:100%;background-color:transparent;">
                                         <div class="col num12" style="min-width:320px;max-width:600px;vertical-align:top;width:600px;">
                                             <div class="col_cont" style="width:100%;">
+                                            <h1 style="line-height:120%;text-align:center;margin-bottom:0px; margin-top:20px;">
+                                                    <font color="#555555" face="Arial, Helvetica Neue, Helvetica, sans-serif">
+                                                        <span style="font-size:36px;">Test Mail</span>
+                                                    </font>
+                                                </h1>
                                                 <div style="color:#262b30;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;line-height:1.2;padding-top:30px;padding-right:5px;padding-bottom:5px;padding-left:5px;">
                                                     <div class="txtTinyMce-wrapper" style="line-height:1.2;font-size:12px;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;color:#262b30;">
                                                         <p style="margin:0;font-size:12px;line-height:1.2;text-align:center;margin-top:0;margin-bottom:0;">
@@ -392,6 +397,7 @@ class CreateSmsTemplatesTable extends Migration
                                     <div style="border-collapse:collapse;width:100%;background-color:transparent;">
                                         <div class="col num12" style="min-width:320px;max-width:600px;vertical-align:top;width:600px;">
                                             <div class="col_cont" style="width:100%;">
+
                                                 <div align="center" class="img-container center autowidth" style="padding-right:20px;padding-left:20px;">
                                                     <img border="0" class="center autowidth" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGF00Oi-zJNU_EvYGueBVz_sqXmFjk8pxNtg&amp;usqp=CAU" style="text-decoration:none;height:auto;border:0;max-width:541px;" width="541" alt="images?q=tbn:ANd9GcRGF00Oi-zJNU_EvYGueBVz_sqXmFjk8pxNtg&amp;usqp=CAU">
                                                 </div>
@@ -480,6 +486,11 @@ class CreateSmsTemplatesTable extends Migration
                                     <div style="border-collapse:collapse;width:100%;background-color:transparent;">
                                         <div class="col num12" style="min-width:320px;max-width:600px;vertical-align:top;width:600px;">
                                             <div class="col_cont" style="width:100%;">
+                                            <h1 style="line-height:120%;text-align:center;margin-bottom:0px;margin-top:20px;">
+                                            <font color="#555555" face="Arial, Helvetica Neue, Helvetica, sans-serif">
+                                                <span style="font-size:36px; margin-top:20px;">Test Mail</span>
+                                            </font>
+                                        </h1>
                                                 <div align="center" class="img-container center autowidth" style="padding-right:20px;padding-left:20px;">
                                                     <img border="0" class="center autowidth" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGF00Oi-zJNU_EvYGueBVz_sqXmFjk8pxNtg&amp;usqp=CAU" style="text-decoration:none;height:auto;border:0;max-width:541px;" width="541" alt="images?q=tbn:ANd9GcRGF00Oi-zJNU_EvYGueBVz_sqXmFjk8pxNtg&amp;usqp=CAU">
                                                 </div>
