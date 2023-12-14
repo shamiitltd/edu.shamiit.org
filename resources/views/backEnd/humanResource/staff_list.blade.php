3bd766633b580ac148909a8194cc5024315faaaa
devangi <devangihrathod@gmail.com>
2023-12-14 17:19:00 +0530

    change for staff search

--- resources/views/backEnd/humanResource/staff_list.blade.php
+++ resources/views/backEnd/humanResource/staff_list.blade.php
@@ -339,15 +339,20 @@
         modal.modal('show');
     }
 </script>
-
 <script>
-        $(document).ready(function () {
-            $('#table_id').DataTable({
-                processing: true,
-                serverSide: true,
-                ajax: "{{ route('staff_directory') }}",
-                columns: [
-                    {data: 'DT_RowIndex', name: 'id'},
+$(document).ready(function() {
+       $('.data-table').DataTable({
+                     processing: true,
+                     serverSide: true,
+                     "ajax": $.fn.dataTable.pipeline( {
+                           url: "{{route('searchStaff')}}",
+                           data: { 
+                            },
+                           pages: "{{generalSetting()->ss_page_load}}" // number of pages to cache
+                           
+                       } ),
+                       columns: [
+                           {data: 'DT_RowIndex', name: 'id'},
                            {data: 'full_name', name: 'full_name'},
                            {data: 'roles.name', name: 'roles.name'},
                            {data: 'departments.name', name: 'departments.name'},
@@ -356,11 +361,87 @@
                            {data: 'email', name: 'email'},
                            {data: 'switch', name: 'switch'},
                            {data: 'action', name: 'action', orderable: false, searchable: true},
-                    // Add other column definitions here
-                    
-                ],
-            });
-        });
-    </script>
-
+                        ],
+                        bLengthChange: false,
+                        bDestroy: true,
+                        language: {
+                            search: "<i class='ti-search'></i>",
+                            searchPlaceholder: window.jsLang('quick_search'),
+                            paginate: {
+                                next: "<i class='ti-arrow-right'></i>",
+                                previous: "<i class='ti-arrow-left'></i>",
+                            },
+                        },
+                        dom: "Bfrtip",
+                        buttons: [{
+                            extend: "copyHtml5",
+                            text: '<i class="fa fa-files-o"></i>',
+                            title: $("#logo_title").val(),
+                            titleAttr: window.jsLang('copy_table'),
+                            exportOptions: {
+                                columns: ':visible:not(.not-export-col)'
+                            },
+                        },
+                        {
+                            extend: "excelHtml5",
+                            text: '<i class="fa fa-file-excel-o"></i>',
+                            titleAttr: window.jsLang('export_to_excel'),
+                            title: $("#logo_title").val(),
+                            margin: [10, 10, 10, 0],
+                            exportOptions: {
+                                columns: ':visible:not(.not-export-col)'
+                            },
+                        },
+                        {
+                            extend: "csvHtml5",
+                            text: '<i class="fa fa-file-text-o"></i>',
+                            titleAttr: window.jsLang('export_to_csv'),
+                            exportOptions: {
+                                columns: ':visible:not(.not-export-col)'
+                            },
+                        },
+                        {
+                            extend: "pdfHtml5",
+                            text: '<i class="fa fa-file-pdf-o"></i>',
+                            title: $("#logo_title").val(),
+                            titleAttr: window.jsLang('export_to_pdf'),
+                            exportOptions: {
+                                columns: ':visible:not(.not-export-col)'
+                            },
+                            orientation: "landscape",
+                            pageSize: "A4",
+                            margin: [0, 0, 0, 12],
+                            alignment: "center",
+                            header: true,
+                            customize: function(doc) {
+                                doc.content[1].margin = [100, 0, 100, 0]; //left, top, right, bottom
+                                doc.content.splice(1, 0, {
+                                    margin: [0, 0, 0, 12],
+                                    alignment: "center",
+                                    image: "data:image/png;base64," + $("#logo_img").val(),
+                                });
+                            },
+                        },
+                        {
+                            extend: "print",
+                            text: '<i class="fa fa-print"></i>',
+                            titleAttr: window.jsLang('print'),
+                            title: $("#logo_title").val(),
+                            exportOptions: {
+                                columns: ':visible:not(.not-export-col)'
+                            },
+                        },
+                        {
+                            extend: "colvis",
+                            text: '<i class="fa fa-columns"></i>',
+                            postfixButtons: ["colvisRestore"],
+                        },
+                    ],
+                    columnDefs: [{
+                        visible: false,
+                    }, ],
+                    responsive: true,
+                });
+            } );
+</script>
 @endpush
