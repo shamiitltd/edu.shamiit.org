60235471ad249d803b97523eb3f3a76a1f4c30ce
devangi <devangihrathod@gmail.com>
2023-12-16 18:57:28 +0530

    change for staff

--- resources/views/backEnd/humanResource/staff_list.blade.php
+++ resources/views/backEnd/humanResource/staff_list.blade.php
@@ -232,110 +232,33 @@
 @push('script')  
 
 <script>
-   $(document).ready(function() {
-       $('.data-table').DataTable({
-                     processing: true,
-                     serverSide: true,
-                     "ajax": $.fn.dataTable.pipeline( {
-                           url: "{{route('staff_directory_ajax')}}",
-                           data: { 
-                            },
-                           pages: "{{generalSetting()->ss_page_load}}" // number of pages to cache
-                           
-                       } ),
-                       columns: [
-                           {data: 'DT_RowIndex', name: 'id'},
-                           {data: 'full_name', name: 'full_name'},
-                           {data: 'roles.name', name: 'roles.name'},
-                           {data: 'departments.name', name: 'departments.name'},
-                           {data: 'designations.title', name: 'designations.title'},
-                           {data: 'mobile', name: 'mobile'},
-                           {data: 'email', name: 'email'},
-                           {data: 'switch', name: 'switch'},
-                           {data: 'action', name: 'action', orderable: false, searchable: true},
-                        ],
-                        bLengthChange: false,
-                        bDestroy: true,
-                        language: {
-                            search: "<i class='ti-search'></i>",
-                            searchPlaceholder: window.jsLang('quick_search'),
-                            paginate: {
-                                next: "<i class='ti-arrow-right'></i>",
-                                previous: "<i class='ti-arrow-left'></i>",
-                            },
-                        },
-                        dom: "Bfrtip",
-                        buttons: [{
-                            extend: "copyHtml5",
-                            text: '<i class="fa fa-files-o"></i>',
-                            title: $("#logo_title").val(),
-                            titleAttr: window.jsLang('copy_table'),
-                            exportOptions: {
-                                columns: ':visible:not(.not-export-col)'
-                            },
-                        },
-                        {
-                            extend: "excelHtml5",
-                            text: '<i class="fa fa-file-excel-o"></i>',
-                            titleAttr: window.jsLang('export_to_excel'),
-                            title: $("#logo_title").val(),
-                            margin: [10, 10, 10, 0],
-                            exportOptions: {
-                                columns: ':visible:not(.not-export-col)'
-                            },
-                        },
-                        {
-                            extend: "csvHtml5",
-                            text: '<i class="fa fa-file-text-o"></i>',
-                            titleAttr: window.jsLang('export_to_csv'),
-                            exportOptions: {
-                                columns: ':visible:not(.not-export-col)'
-                            },
-                        },
-                        {
-                            extend: "pdfHtml5",
-                            text: '<i class="fa fa-file-pdf-o"></i>',
-                            title: $("#logo_title").val(),
-                            titleAttr: window.jsLang('export_to_pdf'),
-                            exportOptions: {
-                                columns: ':visible:not(.not-export-col)'
-                            },
-                            orientation: "landscape",
-                            pageSize: "A4",
-                            margin: [0, 0, 0, 12],
-                            alignment: "center",
-                            header: true,
-                            customize: function(doc) {
-                                doc.content[1].margin = [100, 0, 100, 0]; //left, top, right, bottom
-                                doc.content.splice(1, 0, {
-                                    margin: [0, 0, 0, 12],
-                                    alignment: "center",
-                                    image: "data:image/png;base64," + $("#logo_img").val(),
-                                });
-                            },
-                        },
-                        {
-                            extend: "print",
-                            text: '<i class="fa fa-print"></i>',
-                            titleAttr: window.jsLang('print'),
-                            title: $("#logo_title").val(),
-                            exportOptions: {
-                                columns: ':visible:not(.not-export-col)'
-                            },
-                        },
-                        {
-                            extend: "colvis",
-                            text: '<i class="fa fa-columns"></i>',
-                            postfixButtons: ["colvisRestore"],
-                        },
-                    ],
-                    columnDefs: [{
-                        visible: false,
-                    }, ],
-                    responsive: true,
-                });
-            } );
+    var data = @json($data);
+    var allStaffs = @json($all_staffs);
+    var roles = @json($roles);
+
+    $(document).ready(function () {
+        $('form').submit(function (event) {
+            event.preventDefault();
+
+            $.ajax({
+                url: $(this).attr('action'),
+                type: 'GET',
+                data: $(this).serialize(),
+                success: function (data) {
+                    console.log('Received data:', data);
+
+                    // Assuming data is a JSON representation of the updated table rows
+                    $('tbody').html(data);
+                },
+                error: function (error) {
+                    console.log(error);
+                }
+            });
+        });
+    });
 </script>
+
+   
 <script>
     function deleteStaff(id){
         var modal = $('#deleteStaffModal');
