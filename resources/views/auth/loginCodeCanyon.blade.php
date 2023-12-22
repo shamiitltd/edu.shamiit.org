55bea06b6176534da0a00720558bea99080236e6
mesnehadutta <111640554+mesnehadutta@users.noreply.github.com>
2023-12-22 15:50:04 +0530

    commit

--- resources/views/auth/loginCodeCanyon.blade.php
+++ resources/views/auth/loginCodeCanyon.blade.php
@@ -179,7 +179,7 @@ $css = 'background:' . $login_background->color;
     <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
            type="password" name='password' id="password"
            placeholder="@lang('auth.enter_password')" />
-    <span class="input-group-addon" style="cursor: pointer;" onclick="togglePassword()">
+    <span class="input-group-addon" style="cursor: pointer; left:-40px" onclick="togglePassword()">
         <i class="ti-eye" id="eye-icon"></i>
     </span>
 </div>
