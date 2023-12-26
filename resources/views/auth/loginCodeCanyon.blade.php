fdf912195b9e9007e2e4bbcb96c6e79a8225b0e4
mesnehadutta <111640554+mesnehadutta@users.noreply.github.com>
2023-12-26 18:28:13 +0530

    commit

--- resources/views/auth/loginCodeCanyon.blade.php
+++ resources/views/auth/loginCodeCanyon.blade.php
@@ -176,26 +176,23 @@ $css = 'background:' . $login_background->color;
     <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
            type="password" name='password' id="password"
            placeholder="@lang('auth.enter_password')" />
-    <span class="input-group-addon" style="cursor: pointer; position: relative;">
-        <i class="ti-eye" id="eye-icon" style="position:absolute; top: 20px;margin-left:-30px; transform: translateY(-50%); z-index:4;"></i>
-    </span>
-</div>
-<script>
-    const eyeIcon = document.getElementById('eye-icon');
-    const passwordInput = document.getElementById('password');
 
-    eyeIcon.addEventListener('click', function() {
-        if (passwordInput.type === 'password') {
-            passwordInput.type = 'text';
-            eyeIcon.classList.remove('ti-eye');
-            eyeIcon.classList.add('ti-eye-slash');
-        } else {
-            passwordInput.type = 'password';
-            eyeIcon.classList.remove('ti-eye-slash');
-            eyeIcon.classList.add('ti-eye');
-        }
-    });
-</script>
+           <button type="button" class="flex absolute inset-y-0 right-0 items-center pr-3" @click="show = !show" :class="{'hidden': !show, 'block': show }">
+            <!-- Heroicon name: eye -->
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+        </button>
+        <button type="button" class="flex absolute inset-y-0 right-0 items-center pr-3" @click="show = !show" :class="{'block': !show, 'hidden': show }">
+            <!-- Heroicon name: eye-slash -->
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+            </svg>
+        </button>
+    
+</div>
+
 
 
                                 @if ($errors->has('password'))
