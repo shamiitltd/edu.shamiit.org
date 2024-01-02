be86e45dafef3d34260089a5c5961171793f4c68
mesnehadutta <111640554+mesnehadutta@users.noreply.github.com>
2024-01-02 10:34:07 +0530

    commit

--- resources/views/backEnd/student_leave/apply_leave.blade.php
+++ resources/views/backEnd/student_leave/apply_leave.blade.php
@@ -140,7 +140,6 @@
                            
                         </div>
                         <input type="" name="id" value="{{isset($apply_leave)? $apply_leave->id: ''}}">
-                        <h1>{{ $apply_leave }}</h1>
                         <div class="row mt-25">
                             <div class="col-lg-12">
                                 <select class="primary_select  form-control{{ $errors->has('leave_type') ? ' is-invalid' : '' }}" name="leave_type">
