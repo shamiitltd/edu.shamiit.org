a9c926f808a4d6e7e93408e797dda49ec4fe298e
mesnehadutta <111640554+mesnehadutta@users.noreply.github.com>
2024-01-03 22:15:20 +0530

    commit

--- resources/views/backEnd/student_leave/apply_leave.blade.php
+++ resources/views/backEnd/student_leave/apply_leave.blade.php
@@ -292,7 +292,7 @@
                             <tr>
                                 <td>
                                     @if($apply_leave->leaveDefine != "" && $apply_leave->leaveDefine->leaveType !="")
-                                        {{$apply_leave->leave_define_id->leaveType->type}}
+                                        {{$apply_leave->leave_define_id->leave_type->type}}
                                     @endif
                                 </td>
                                 <td  data-sort="{{strtotime($apply_leave->leave_from)}}" >
