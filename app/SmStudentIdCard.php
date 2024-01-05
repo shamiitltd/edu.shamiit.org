f9e622132ffed9ff3e735d951df84139264699c7
Mohit Yadav <mohityadav12181@gmail.com>
2024-01-05 17:52:54 +0530

    nothing

--- app/SmStudentIdCard.php
+++ app/SmStudentIdCard.php
@@ -23,7 +23,8 @@ class SmStudentIdCard extends Model
             $names=[];
             $value=json_decode($id_card->role_id,true);
             foreach($value as $values){
-                $arr[] = $value;
+                 $arr[] = $value; 
+                 //nothing
             }
             $roleNames = Role::whereIn('id',$arr)->get(['id','name']);
         return $roleNames;
