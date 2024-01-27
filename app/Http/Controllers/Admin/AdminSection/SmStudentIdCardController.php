810b2dafb1fa5644cf62b3efc77a4ce5ab532ef5
Sweeti Agrahari <sweetiagrmit10@gmail.com>
2024-01-27 21:33:10 +0530

    debug

--- app/Http/Controllers/Admin/AdminSection/SmStudentIdCardController.php
+++ app/Http/Controllers/Admin/AdminSection/SmStudentIdCardController.php
@@ -29,7 +29,7 @@ class SmStudentIdCardController extends Controller
     public function index()
     {
         try {
-            $id_cards = SmStudentIdCard::where('active_status', 1)->where('school_id', Auth::user()->school_id)->get();
+            $id_cards = SmStudentIdCard::where('active_status', 1)->where('school_id', Auth::user()->school_id)->
             dd($id_cards);
             return view('backEnd.admin.idCard.student_id_card_list',compact('id_cards'));
         } catch (\Exception $e) {
