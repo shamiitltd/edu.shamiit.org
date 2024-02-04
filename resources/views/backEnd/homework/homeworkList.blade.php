b30e5add88dd161d224db43a4b9018436c408410
Mohit Yadav <mohityadav12181@gmail.com>
2024-02-04 21:47:15 +0530

    ccc

--- resources/views/backEnd/homework/homeworkList.blade.php
+++ resources/views/backEnd/homework/homeworkList.blade.php
@@ -23,7 +23,7 @@
         <div class="row">
             <div class="col-lg-8 col-md-6">
                 <div class="main-title">
-                    <h3 class="mb-34">@lang('common.select_criteria') </h3>
+                    <h3 class="mb-33">@lang('common.select_criteria') </h3>
                 </div>
             </div>
             <div class="col-lg-4 text-md-right text-left col-md-6 mb-30-lg">
