2aa9cc3253a56dffbc126256993b56f588a10b18
devangi <devangihrathod@gmail.com>
2024-02-12 16:25:12 +0530

    cmd

--- resources/views/backEnd/parentPanel/parent_dashboard.blade.php
+++ resources/views/backEnd/parentPanel/parent_dashboard.blade.php
@@ -111,7 +111,7 @@
                             </a>
                         </div>
                     @endif
-                    @if (userPermission('om_parent_online_examination'))
+                    @if (userPermission('exam'))
                         <div class="col-lg-3 col-md-6">
                             <a href="{{ route('parent_online_examination', $children->id) }}" class="d-block">
                                 <div class="white-box single-summery">
