8137fe84f9fd3f4cb3eec60fb681e66c406f06d2
devangi <devangihrathod@gmail.com>
2024-02-12 16:14:06 +0530

    cmd

--- resources/views/backEnd/parentPanel/parent_dashboard.blade.php
+++ resources/views/backEnd/parentPanel/parent_dashboard.blade.php
@@ -111,26 +111,10 @@
                             </a>
                         </div>
                     @endif
-                    @if (userPermission('parent-dashboard-exam'))
-                        <!-- <div class="col-lg-3 col-md-6">
-                            <a href="{{ route('parent_online_examination', $children->id) }}" class="d-block">
-                                <div class="white-box single-summery">
-                                    <div class="d-flex justify-content-between">
-                                        <div>
-                                            <h3>@lang('parent.online_exam')</h3>
-                                            <p class="mb-0">@lang('parent.total_online_exam')</p>
-                                        </div>
-                                        <h1 class="gradient-color2">
-
-                                                {{ $totalOnlineExams }}
-                                        </h1>
-                                    </div>
-                                </div>
-                            </a>
-                        </div> -->
+                    @if (userPermission('om_parent_online_examination'))
                         <div class="col-lg-3 col-md-6">
                             <a href="{{ route('parent_online_examination', $children->id) }}" class="d-block">
-                            <div class="white-box single-summery">
+                                <div class="white-box single-summery">
                                     <div class="d-flex justify-content-between">
                                         <div>
                                             <h3>@lang('parent.online_exam')</h3>
