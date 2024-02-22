df74f1ae6546fa92d5c17287056d82b3b2aa15fe
Mohit Yadav <mohityadav12181@gmail.com>
2024-02-22 21:18:22 +0530

    fixing  total  field

--- resources/views/backEnd/examination/exam.blade.php
+++ resources/views/backEnd/examination/exam.blade.php
@@ -212,11 +212,11 @@
                                     <tfoot>
                                         <tr>
                                             <td class="border-top-0" style="position: relative; top: 12px">
-                                                @lang('exam.total')  <td class="border-top-0" id="totalMark">
+                                                @lang('exam.total') </td> <td class="border-top-0" id="totalMark">
                                                 <input type="text" class="primary_input_field form-control"
                                                     name="totalMark" readonly="true">
                                              </td>     
-                                            </td>
+                                            
                                               <td class="border-0">
                                                 <button style="position: relative; top: 6px;"
                                                     class="primary-btn icon-only fix-gr-bg" type="button">
